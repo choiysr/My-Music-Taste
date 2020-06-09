3702d35bf4e9f90d75a4c005d80fa2b6e6e5a7bd
hyunbin <hyunbin7195@daum.net>
2020-06-09 16:01:32 +0900

    kakao api

--- src/main/java/com/pmg/mymusictaste/controller/BaseController.java
+++ src/main/java/com/pmg/mymusictaste/controller/BaseController.java
@@ -1,20 +1,41 @@
 package com.pmg.mymusictaste.controller;
 
+import com.pmg.mymusictaste.service.KakaoLoginService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BaseController {
 
+  @Autowired
+  KakaoLoginService kakao;
+
 
   @GetMapping("/")
   public String mappedUrl() {
-    return "indexTest";
+    return "login";
   }
 
   @GetMapping("/musicTest")
   public String mmapedUrl2(){
     return "musicTest";
   } 
+  @GetMapping("/indexTest")
+  public String mmapedUrl3(){
+    return "indexTest";
+  }
+
+  @GetMapping("/oauth")
+  public String mmapedUrl4(@RequestParam String code){
+    System.out.println("code > " + code);
+    String access_Token = kakao.getAccessToken(code);
+    System.out.println("accesS_Token :" +access_Token);
+    return "redirect:/indexTest";
+  }
+
+
   
 }
